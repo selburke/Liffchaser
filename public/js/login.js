6ed5e91e2d261a4088fab282d5d162c373d36e98
Ricky <rickyjoet@gmail.com>
2021-07-06 11:06:52 -0700

    updated html

--- public/js/login.js
+++ public/js/login.js
@@ -28,8 +28,9 @@ const signupFormHandler = async (event) => {
   const name = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-
+  
   if (name && email && password) {
+
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify({ name, email, password }),
