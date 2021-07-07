42632287ab94bb0622a88bfe3c9758a487e661f0
Alex Gihts <githenyalex@gmail.com>
2021-07-06 19:16:46 -0700

    added profile

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,12 +1,12 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
+    const name = document.querySelector('#company').value.trim();
+    const needed_funding = document.querySelector('#position').value.trim();
+    const description = document.querySelector('#job-desc').value.trim();
   
     if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
+      const response = await fetch(`/api/jobs`, {
         method: 'POST',
         body: JSON.stringify({ name, needed_funding, description }),
         headers: {
@@ -26,23 +26,23 @@ const newFormHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/projects/${id}`, {
+      const response = await fetch(`/api/jobs/${id}`, {
         method: 'DELETE',
       });
   
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to delete project');
+        alert('Failed to delete job');
       }
     }
   };
   
   document
-    .querySelector('.new-project-form')
+    .querySelector('.new-job-form')
     .addEventListener('submit', newFormHandler);
   
   document
-    .querySelector('.project-list')
+    .querySelector('.job-list')
     .addEventListener('click', delButtonHandler);
   
