ca07a93483cb8c5d3becfe40f1543a89bd13eb87
Alex Gihts <githenyalex@gmail.com>
2021-07-08 13:01:08 -0700

    timerchanges

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,4 @@
 const logout = async () => {
-<<<<<<< HEAD
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -14,18 +13,3 @@ const logout = async () => {
   
   document.querySelector('#logout').addEventListener('click', logout);
   
-=======
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert(response.statusText);
-  }
-};
-
-document.querySelector('#logout').addEventListener('click', logout);
->>>>>>> 470286192b1cdf719eb149e6c1308d4ecf392d3a
